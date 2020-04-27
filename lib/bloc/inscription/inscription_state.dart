ffb00c8d15465df79511e35b09c4901bb428b28f
Aymeroc <aympab@gmail.com>
2020-04-27 09:27:49 +0200

    event et etats complets

--- lib/bloc/inscription/inscription_state.dart
+++ lib/bloc/inscription/inscription_state.dart
@@ -1,26 +1,36 @@
 part of 'inscription_bloc.dart';
 
 abstract class InscriptionState extends Equatable {
-  const InscriptionState();
 }
 
+//Etat initial donc quand on arrive sur la page inscription et qu'on est en train d'entrer les données
 class InscriptionInitial extends InscriptionState {
   @override
-  List<Object> get props => [];
-}
-
-class AddingNewUserState extends InscriptionState{
-
-
-  @override
-  // TODO: implement props
   List<Object> get props => null;
 }
 
+//Lorsqu'on clique sur le bouton créer un compte et qu'on est en train d'accéder à firebase etc, CHARGEMENT
+class AddingNewUserState extends InscriptionState{
+  User user;
+
+  @override
+  List<Object> get props => [this.user];
+}
+
+//Si on a une erreur lors de l'ajout
 class ErrorWhenAddingState extends InscriptionState{
+  User user;
+  String messageErreur;
+
+  @override
+  List<Object> get props => [this.user, this.messageErreur];
 
 }
 
-classe NewUserAddedState extends InscriptionState{
-  
+//Quand l'user à bien été ajouté à la BD
+class NewUserAddedState extends InscriptionState{
+  User user;
+
+  @override
+  List<Object> get props => [this.user];  
 }
