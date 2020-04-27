dda9b9429a887d88c0c366f9a1aefa960671a7f6
Aymeroc <aympab@gmail.com>
2020-04-27 09:51:17 +0200

    Mise en place des classes besoin et de la classe user //FIXME dans le constructeur

---
+++ lib/model/besoins.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Besoin extends Equatable {
+  String nom;
+  int nombreUtilise;
+
+  Besoin({this.nom, this.nombreUtilise});
+
+  void incrNombreUtilise() => this.nombreUtilise++;
+}
+
+class Competence extends Besoin{
+  //FIXME : Ici ça va pas le constructeur
+  Competence(nom, nombreUtilise) : super(nom, nombreUtilise);
+
+  @override
+  List<Object> get props => [this.nom, this.nombreUtilise];
+}
+
+class Materiel extends Besoin{
+  @override
+  List<Object> get props => [this.nom, this.nombreUtilise];
+}
+
+class Contact extends Besoin{
+  @override
+  List<Object> get props => [this.nom, this.nombreUtilise];
+}
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,16 @@
 class User{
+
+  String pseudo;
+  String email;
+  String password;
+  DateTime dateNaissance;
+
+  InformationsFacultativesUser infosFacultatives;
   
+}
+  
+class InformationsFacultativesUser {
+  String prenom;
+  String nom;
+  String zoneGeographique;
 }
