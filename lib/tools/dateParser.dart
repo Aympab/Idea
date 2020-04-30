9d9ec95a1a835d203de571a1ac7077dd2c96b403
Aymeroc <aympab@gmail.com>
2020-04-30 13:06:14 +0200

    dateParser implemented

--- lib/tools/dateParser.dart
+++ lib/tools/dateParser.dart
@@ -1,1 +1,15 @@
-//TODO : Implement date parser
+abstract class DateParser {
+
+  //Parse un string de la forme jj/mm/aaaa au format DateTime
+  static DateTime parseStringToDateTime(String strDate) {
+    DateTime date;
+
+    String jj = strDate.substring(0, 2);
+    String mm = strDate.substring(3, 5);
+    String aaaa = strDate.substring(6, 10);
+
+    date = DateTime.parse('$aaaa$mm$jj');
+
+    return date;
+  }
+}
