4a8e95ce311f8d7df98b4ac31ed8a465176ca0d1
Aymeroc <aympab@gmail.com>
2020-05-02 16:52:10 +0200

    buttons for differents pages

--- lib/view/profile/profile.dart
+++ lib/view/profile/profile.dart
@@ -13,62 +13,84 @@ class ProfileViewState extends State<ProfileView> {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
-        const SliverAppBar(
-          floating: false,
-          pinned: true,
-          snap: false,
-          expandedHeight: 300.0,
-          flexibleSpace: ProfileHeader()//FlexibleSpaceBar(title: Text('Demo'),),
-        ),
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            childAspectRatio: 4.0,
-          ),
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.teal[100 * (index % 9)],
-                child: Text('Grid Item $index'),
-              );
-            },
-            childCount: 20,
-          ),
-        ),
-        SliverFixedExtentList(
-          itemExtent: 50.0,
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.lightBlue[100 * (index % 9)],
-                child: Text('List Item $index'),
-              );
-            },
-          ),
-        ),
+        _buildAppBar(),
+        _buildSilverGridTabs(),
+
+        // SliverFixedExtentList(
+        //   itemExtent: 50.0,
+        //   delegate: SliverChildBuilderDelegate(
+        //     (BuildContext context, int index) {
+        //       return Container(
+        //         alignment: Alignment.center,
+        //         color: Colors.lightBlue[100 * (index % 9)],
+        //         child: Text('List Item $index'),
+        //       );
+        //     },
+        //   ),
+        // ),
       ],
     );
+  }
 
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.amber[100],
-        body: Column(
+  Widget _buildAppBar() {
+    return const SliverAppBar(
+        floating: false,
+        pinned: true,
+        snap: false,
+        expandedHeight: 300.0,
+        flexibleSpace: ProfileHeader() //FlexibleSpaceBar(title: Text('Demo'),),
+        );
+  }
+
+  Widget _buildIconButton(IconData icon, String text, Function onPressedFunc) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Expanded(
-              flex: 40,
-              child: ProfileHeader(),
+            IconButton(
+              icon: Icon(icon),
+              onPressed: () {
+                print('Tapped');
+              },
+              //onPressed: onPressedFunc,
             ),
-            Expanded(
-              flex: 60,
-              child: ProfileTabs(),
-            ),
+            Text('$text')
           ],
         ),
       ),
     );
   }
+
+  _buildSilverGridTabs() {
+    final List<Widget> _buttonsTab = [
+      _buildIconButton(Icons.info, 'Infos', () {}),
+      _buildIconButton(Icons.lightbulb_outline, 'Idées', () {}),
+      _buildIconButton(Icons.graphic_eq, 'Récap', () {}),
+      _buildIconButton(Icons.bubble_chart, 'Ressources', () {}),
+    ];
+
+    return SliverGrid(
+      delegate: SliverChildListDelegate(
+        _buttonsTab,
+      ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 100.0,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        childAspectRatio: 1.0,
+      ),
+    );
+  }
 }
+
+// SliverChildBuilderDelegate(
+//         (BuildContext context, int index) {
+//           return Container(
+//             alignment: Alignment.center,
+//             color: Colors.teal[100 * (index % 9)],
+//             child: Text('Grid Item $index'),
+//           );
+//         },
+//         childCount: 4,
+//       ),
