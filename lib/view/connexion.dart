2f767a4a252463c3e25bc510cd08b8366cc98c11
Aymeroc <aympab@gmail.com>
2020-05-04 20:46:39 +0200

    minor UI updates

--- lib/view/connexion.dart
+++ lib/view/connexion.dart
@@ -10,82 +10,82 @@ class ConnexionView extends StatefulWidget {
   _ConnexionViewState createState() => _ConnexionViewState();
 }
 
-class _ConnexionViewState extends State<ConnexionView>{
-
+class _ConnexionViewState extends State<ConnexionView> {
   Image ideaLogo = new Image.asset('assets/IdeaSurAmpoule.png');
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    FlatButton connexionButton = new FlatButton(
+    FlatButton connexionButton = buildFlatButton("Connexion");
+
+    FlatButton continueWithoutConnexionButton =
+        buildFlatButton("Continuer \nsans \ns'inscrire");
+
+    FlatButton inscriptionButton = buildFlatButton("Inscription");
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new SizedBox(height: 32),
+            new Row(
+              children: <Widget>[
+                new Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
+                  child: ideaLogo,
+                )),
+              ],
+            ),
+            new SizedBox(height: 40),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Row(
+                      children: <Widget>[
+                        connexionButton,
+                        new SizedBox(width: 45),
+                        continueWithoutConnexionButton,
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: new Row(
+                        children: <Widget>[
+                          inscriptionButton,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  FlatButton buildFlatButton(String text) {
+    return FlatButton(
       color: Colors.yellow,
-      padding: EdgeInsets.all(40.0),
+      // padding: EdgeInsets.fromLTRB(40, 25, 40, 25),
       onPressed: () {},
-      child: new Text("Connexion",
-        style: TextStyle(fontWeight: FontWeight.bold),
-        overflow: TextOverflow.ellipsis,
-      ),
-    );
-
-    FlatButton continueWithoutConnexionButton = new FlatButton(
-      color: Colors.yellow,
-      padding: EdgeInsets.fromLTRB(40, 25, 40, 25),
-      onPressed: () {},
-      child: new Text("Continuer \nsans \ns'inscrire",
-        style: TextStyle(fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
-        overflow: TextOverflow.ellipsis,
-      ),
-    );
-
-    FlatButton inscriptionButton = new FlatButton(
-      color: Colors.yellow,
-      padding: EdgeInsets.all(40.0),
-      onPressed: () {
-
-      },
-      child: new Text("Inscription",
-        style: TextStyle(fontWeight: FontWeight.bold),
-        overflow: TextOverflow.ellipsis,
-      ),
-    );
-
-    return new Scaffold(
-      body: new Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          new SizedBox(height: 32),
-          new Row(
-            children: <Widget>[
-              new Expanded(child: ideaLogo),
-            ],
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
           ),
-          new SizedBox(height: 64),
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Column(
-                children: <Widget>[
-                  new Row(
-                    children: <Widget>[
-                      connexionButton,
-                      new SizedBox(width: 64),
-                      continueWithoutConnexionButton,
-                    ],
-                  ),
-                  new SizedBox(height : 64),
-                  new Row(
-                    children: <Widget>[
-                      inscriptionButton,
-                    ],
-                  ),
-                  new SizedBox(height: 16),
-                ],
-              ),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
